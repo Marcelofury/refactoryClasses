48d0133b07ae3b50f353349a92c5c3121c1920fe
Marcelofury <marcelofury13579@gmail.com>
2026-01-08 12:37:49 +0300

    updates class.js

--- Day 14/Classes and OOP/class.js
+++ Day 14/Classes and OOP/class.js
@@ -4,3 +4,16 @@ class Car {
     engine='V6';
     topSpeed=200;
 }
+
+
+//create an object with a class sale and show how to use a constructor to initialize the properties
+class Sale {
+    constructor(item, price, quantity) {
+        this.item = item;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    totalCost() {
+        return this.price * this.quantity;
+    }
+}
