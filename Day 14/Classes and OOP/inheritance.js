f1a8d1f446fc29a4512297e3d26d2a96029c17b6
Marceofury <marcelofury13579@gmail.com>
2026-01-09 11:44:33 +0300

    added inheritance implementations

--- Day 14/Classes and OOP/inheritance.js
+++ Day 14/Classes and OOP/inheritance.js
@@ -28,6 +28,7 @@ let sakeProcurement = new KGLProcurement("Sake", 2000, 5000, "2024-06-15", "USD"
 console.log(sakeProcurement.getDetails()); 
 // Output: Produce: Sake, Tonnage(kg): 2000, Total Cost: 5000 USD, Sales Date: 2024-06-15   
 
-let creditSale = new KGLCreditSale("Coffee", 1500, 3000, "2024-06-20", "USD");
+let creditSale = new KGLCreditSale("Coffee", 1500, 3000, "2024-06-20", "USD", true, "2024-07-20");
 console.log(creditSale.getDetails()); 
 // Output: Produce: Coffee, Tonnage(kg): 1500, Total Cost: 3000 USD, Sales Date: 2024-06-20 
+
