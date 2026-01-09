6384ea232840700163b94206c692aaf292163292
Marcelofury <marcelofury13579@gmail.com>
2026-01-09 12:31:04 +0300

    created inheritance exercise1

---
+++ Day 15/exercise1.js
@@ -0,0 +1,25 @@
+class SalesAgent{
+constructor(id,name,branch,salary,yearsOfExperience){
+    this.id = id;
+    this.name = name;
+    this.branch = branch;
+    this.salary = salary;
+    this.yearsOfExperience = yearsOfExperience;
+}
+}
+saleProduct(productName){
+    console.log(`${this.name} sold ${productName}`);
+
+
+}
+
+
+
+class Manager{
+
+
+
+    CanRecordProcurement(){
+
+}
+}
