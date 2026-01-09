c30111d3ebbbc0fbe0c32d0fee17ffc3d084a41f
Marceofury <marcelofury13579@gmail.com>
2026-01-09 11:39:58 +0300

    updated inheritace OOP structure

--- Day 14/Classes and OOP/inheritance.js
+++ Day 14/Classes and OOP/inheritance.js
@@ -12,8 +12,22 @@ class KGLProcurement {
     }
 }
 
+class KGLCreditSale extends KGLProcurement {
+    constructor(nameOfProduce, tonnageInkgs,totalCost,salesDate,currency, isCreditSale,dueDate){
+        //calls the parent class constructor
+        super(nameOfProduce, tonnageInkgs,totalCost,salesDate,currency);
+        //additional properties for the child class
+        this.isCreditSale = true;
+        this.dueDate = dueDate;
+
+    }
+}
+
+
 let sakeProcurement = new KGLProcurement("Sake", 2000, 5000, "2024-06-15", "USD");
 console.log(sakeProcurement.getDetails()); 
 // Output: Produce: Sake, Tonnage(kg): 2000, Total Cost: 5000 USD, Sales Date: 2024-06-15   
 
-
+let creditSale = new KGLCreditSale("Coffee", 1500, 3000, "2024-06-20", "USD");
+console.log(creditSale.getDetails()); 
+// Output: Produce: Coffee, Tonnage(kg): 1500, Total Cost: 3000 USD, Sales Date: 2024-06-20 
