34b6412534967b370abd5ea56f3a05ed47184aa2
Marceofury <marcelofury13579@gmail.com>
2026-01-21 15:19:52 +0300

    added getElementByTagName DOM manipulation

---
+++ Day 21 (DOM intro & selection)/dom.js
@@ -0,0 +1,9 @@
+//getElementById example
+
+// HTML: <div id="total-dislay"></div>
+
+const el = document.getElementById("total-dsilay");
+
+
+//getElementBytagName example
+const heading = document.getElementsByTagName("h1");
