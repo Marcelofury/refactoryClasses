5ef2647d2f8c9651d6815f8c96094d396fc887c2
Marceofury <marcelofury13579@gmail.com>
2026-01-21 16:27:09 +0300

    updated changeContent js file

--- Day 22 (Manipulation [changing content/changeContent.js
+++ Day 22 (Manipulation [changing content/changeContent.js
@@ -1,1 +1,8 @@
-const el = document.querySelector("#title")
+//Element.textContent
+const el = document.querySelector("#title");
+
+// 1.Get
+console.log(el.textContent)
+
+// 2.Set
+el.textContent = "New Title";
